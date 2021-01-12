736bc24936688b2eca1f00bd74842fc4809ee9e8
yashibajpai <71567118+yashibajpai@users.noreply.github.com>
2021-01-11 21:03:36 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Project_21
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,45 @@
+var bullet, wall, thickness;
+var speed, weight
+
+function setup() {
+  createCanvas(1600,400);
+
+  speed=random(223, 321)
+  weight=random(30,52)
+  thickness=random(22,83)
+    
+  bullet=createSprite(50, 200, 50, 50)
+  bullet.shapeColor = "white"
+
+  wall=createSprite(1200, 200, thickness,height/2)
+  wall.shapeColor = color(80,80,80)
+
+  bullet.velocityX = speed
+
+}
+
+function draw() {
+  background(255,0,255); 
+  
+  if(hasCollided(bullet, wall)) {
+    bullet.velocityX=0;
+    var damage = 0.5 *weight * speed * speed/(thickness * thickness *thickness);
+    if(damage>10) {
+      wall.shapeColor=color(255,0,0);
+    }
+    if(damage<10) {
+      wall.shapeColor=color(0,255,0)
+    }
+  }
+
+  drawSprites();
+}
+
+function hasCollided(Lbullet, Lwall) {
+  bulletRightEdge=Lbullet.x + Lbullet.width;
+  wallLeftEdge=Lwall.x;
+  if(bulletRightEdge>=wallLeftEdge) {
+    return true
+  }
+  return false
+}
